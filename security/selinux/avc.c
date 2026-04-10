c778927f63634f4766694d32550670bc63f61e63
nhglong <its.nhglong@gmail.com>
2026-04-10 12:35:36 +0700

    treewide: Remove samsung selinux implementation

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -1017,13 +1017,9 @@ static noinline int avc_denied(struct selinux_state *state,
 	if (flags & AVC_STRICT)
 		return -EACCES;
 
-#ifdef CONFIG_ALWAYS_ENFORCE
-	if (!(avd->flags & AVD_FLAGS_PERMISSIVE))
-#else
-	if (selinux_enforcing && !(avd->flags & AVD_FLAGS_PERMISSIVE))
-#endif
+	if (enforcing_enabled(state) &&
+	    !(avd->flags & AVD_FLAGS_PERMISSIVE))
 		return -EACCES;
-// ] SEC_SELINUX_PORTING_COMMON
 
 	avc_update_node(state->avc, AVC_CALLBACK_GRANT, requested, driver,
 			xperm, ssid, tsid, tclass, avd->seqno, NULL, flags);
