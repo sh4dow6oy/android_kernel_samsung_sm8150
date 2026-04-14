b00d0a793f376b39c24c4951e154050c5387c174
nhglong <its.nhglong@gmail.com>
2026-04-15 00:56:14 +0700

    net: Remove multi path tcp

--- include/net/sock.h
+++ include/net/sock.h
@@ -787,9 +787,6 @@ enum sock_flags {
 	SOCK_FILTER_LOCKED, /* Filter cannot be changed anymore */
 	SOCK_SELECT_ERR_QUEUE, /* Wake select on error queue */
 	SOCK_RCU_FREE, /* wait rcu grace period in sk_destruct() */
-#ifdef CONFIG_MPTCP
-	SOCK_MPTCP, /* MPTCP set on this socket */
-#endif
 };
 
 #define SK_FLAGS_TIMESTAMP ((1UL << SOCK_TIMESTAMP) | (1UL << SOCK_TIMESTAMPING_RX_SOFTWARE))
@@ -1090,9 +1087,7 @@ struct proto {
 	void			(*unhash)(struct sock *sk);
 	void			(*rehash)(struct sock *sk);
 	int			(*get_port)(struct sock *sk, unsigned short snum);
-#ifdef CONFIG_MPTCP
-	void			(*clear_sk)(struct sock *sk, int size);
-#endif
+
 	/* Keeping track of sockets in use */
 #ifdef CONFIG_PROC_FS
 	unsigned int		inuse_idx;
